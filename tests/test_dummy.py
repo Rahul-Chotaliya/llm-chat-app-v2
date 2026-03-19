f33f011fb7b9e90d74da201cfad43547429deec6
Rahul-Chotaliya <rahulchotliya18@gmail.com>
2026-03-19 15:00:08 +0530

    added fix with namming convension

---
+++ tests/test_dummy.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def test_dummy():
+    assert True
+
+
+def test_health_check():
+    response = requests.get("http://localhost:8000/health")
+    assert response.status_code == 200
